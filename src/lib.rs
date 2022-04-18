1599358a9125aaee4812c8e60160ab5724450d64
Alex Laurent <shaoner@pm.me>
2022-04-18 09:55:10 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use wasm_bindgen::prelude::*;
+
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn main() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+
+    #[cfg(feature = "console_log")]
+    #[cfg(debug_assertions)]
+    {
+        use log::Level;
+        console_log::init_with_level(Level::Debug).expect("error initializing log");
+    }
+}
