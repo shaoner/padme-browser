b5f18a91f13e5ef5d36c14799ac7e5e96723d1b1
Alex Laurent <shaoner@pm.me>
2022-04-20 15:50:45 -0400

    Rework global settings to be on a table to have proper alignment

--- www/src/components/settings/settings.tsx
+++ www/src/components/settings/settings.tsx
@@ -19,101 +19,113 @@ const Settings: FunctionalComponent<Props> = ({ isOpen, onClose }) => {
     return (
         <Modal title={'Settings'} isOpen={isOpen} onClose={onClose}>
             <div class={style.container}>
-            <div class="row">
-                <div class="four columns">
-                    <label for="scale-input">Scale</label>
-                    <input type="range" onInput={(e) => {
-                        e.target && dispatch('game/scale/update', (e.target as HTMLInputElement).value)
-                    }} step={0.1} min={1} max={3} id="scale-input" value={scale} />
-                </div>
-                <div class="four columns">
-                    <label for="style-input">Style</label>
-                    <select id="style-input" value={styleName} onChange={(e) => {
-                       e.target && dispatch('game/style/update', (e.target as HTMLSelectElement).value)
-                    }}>
-                        <option value="nostyle">No style</option>
-                        <option value="classic">Retro</option>
-                    </select>
-                </div>
-                <div class="four columns">
-                    <label for="max-fps-input">Max FPS</label>
-                    <input id="max-fps-input" type="number" onInput={(e) => {
-                        e.target && dispatch('game/max-fps/update', (e.target as HTMLInputElement).value)
-                    }} step={1} min={1} max={200} value={maxFps} />
-                </div>
+                <table class={style.global}>
+                    <tr>
+                        <td>
+                            <label for="scale-input">Scale</label>
+                        </td>
+                        <td>
+                            <input type="range" onInput={(e) => {
+                                e.target && dispatch('game/scale/update', (e.target as HTMLInputElement).value)
+                            }} step={0.1} min={1} max={3} id="scale-input" value={scale} />
+                        </td>
+                        <td>
+                            <label for="style-input">Style</label>
+                        </td>
+                        <td>
+                            <select id="style-input" value={styleName} onChange={(e) => {
+                                e.target && dispatch('game/style/update', (e.target as HTMLSelectElement).value)
+                            }}>
+                                <option value="nostyle">No style</option>
+                                <option value="classic">Retro</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="max-fps-input">Max FPS</label>
+                        </td>
+                        <td>
+                            <input id="max-fps-input" type="number" onInput={(e) => {
+                                e.target && dispatch('game/max-fps/update', (e.target as HTMLInputElement).value)
+                            }} step={1} min={1} max={200} value={maxFps} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+                <div class={style.separator}/>
+                <table class={style.keymap} style={{ display: isMobile() ? 'none' : 'block' }}>
+                    <tr>
+                        <td>
+                            <label class={style.controldir} for="up-input">
+                                <div class={`${style.pointer} ${style.up}`}/>
+                            </label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Up]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Up, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controldir} for="left-input">
+                                <div class={`${style.pointer} ${style.left}`}/>
+                            </label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Left]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Left, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controldir} for="right-input">
+                                <div class={`${style.pointer} ${style.right}`}/>
+                            </label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Right]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Right, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controldir} for="down-input">
+                                <div class={`${style.pointer} ${style.down}`}/>
+                            </label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Down]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Down, code })} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label class={style.controlaction} for="a-input">A</label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.A]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.A, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controlaction} for="b-input">B</label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.B]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.B, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controls} for="select-input">SELECT</label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Select]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Select, code })} />
+                        </td>
+                        <td>
+                            <label class={style.controls} for="start-input">START</label>
+                        </td>
+                        <td class={style.input}>
+                            <InputKey code={keys[WButton.Start]}
+                                      onKey={(code) => dispatch('game/key/update', { name: WButton.Start, code })} />
+                        </td>
+                    </tr>
+                </table>
             </div>
-            <div class={style.separator}/>
-            <table class={style.keymap} style={{ display: isMobile() ? 'none' : 'block' }}>
-                <tr>
-                <td>
-                    <label class={style.controldir} for="up-input">
-                        <div class={`${style.pointer} ${style.up}`}/>
-                   </label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Up]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Up, code })} />
-                </td>
-                <td>
-                    <label class={style.controldir} for="left-input">
-                        <div class={`${style.pointer} ${style.left}`}/>
-                    </label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Left]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Left, code })} />
-                </td>
-                <td>
-                    <label class={style.controldir} for="right-input">
-                        <div class={`${style.pointer} ${style.right}`}/>
-                    </label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Right]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Right, code })} />
-                </td>
-                <td>
-                    <label class={style.controldir} for="down-input">
-                        <div class={`${style.pointer} ${style.down}`}/>
-                    </label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Down]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Down, code })} />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class={style.controlaction} for="a-input">A</label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.A]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.A, code })} />
-                </td>
-                <td>
-                    <label class={style.controlaction} for="b-input">B</label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.B]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.B, code })} />
-                </td>
-                <td>
-                    <label class={style.controls} for="select-input">SELECT</label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Select]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Select, code })} />
-                </td>
-                <td>
-                    <label class={style.controls} for="start-input">START</label>
-                </td>
-                <td class={style.input}>
-                    <InputKey code={keys[WButton.Select]}
-                              onKey={(code) => dispatch('game/key/update', { name: WButton.Start, code })} />
-                </td>
-           </tr>
-        </table>
-        </div>
         </Modal>
     );
 }
