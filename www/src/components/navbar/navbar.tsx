65b7918e9236874e832d99e49de08340a82fa571
Alex Laurent <shaoner@pm.me>
2022-04-22 21:09:53 -0400

    Refacto navbar component

--- www/src/components/navbar/navbar.tsx
+++ www/src/components/navbar/navbar.tsx
@@ -4,18 +4,39 @@ import { useState } from 'preact/hooks';
 import { Help } from '../help';
 import { Settings } from '../settings';
 
-import style from './navbar.scss';
-
 const NavBar: FunctionalComponent = () => {
     const [settingsOpen, setSettingsOpen] = useState(false);
     const [helpOpen, setHelpOpen] = useState(false);
+    const [menuActive, showMenu] = useState(false);
 
     return (
-        <nav class={style.navbar}>
-            <a class={style.brand} href="#">Padme</a>
-            <div class={style.rightmenu}>
-                <a href="#" onClick={() => setHelpOpen(true)}><i class="icon icon-question"></i></a>
-                <a href="#" onClick={() => setSettingsOpen(true)}><i class="icon icon-cog"/></a>
+        <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+            <div class="navbar-menu"></div>
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <img src={require('./logo.svg')} />
+                </a>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" onClick={() => showMenu(!menuActive)}>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class={`navbar-menu ${menuActive ? 'is-active' : ''}`}>
+                <div class="navbar-start">
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <a href="#" onClick={() => setHelpOpen(true)}>
+                            <span class="icon"><i class="padme icon-question"></i></span>
+                        </a>
+                    </div>
+                    <div class="navbar-item">
+                        <a href="#" onClick={() => setSettingsOpen(true)}>
+                            <span class="icon"><i class="padme icon-cog"></i></span>
+                        </a>
+                    </div>
+                </div>
             </div>
             <Settings isOpen={settingsOpen} onClose={() => setSettingsOpen(false)} />
             <Help isOpen={helpOpen} onClose={() => setHelpOpen(false)} />
