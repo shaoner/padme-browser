8a01420a567137bc060aa5cf1f2b07388763f3ed
Alex Laurent <shaoner@pm.me>
2022-04-24 10:29:42 -0400

    Call init main function to init panics & logs

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,6 +1,9 @@
-import { h, render } from "preact";
+import { main } from 'padme-wasm';
+import { h, render } from 'preact';
 import App from './components/app';
 
 import './style/index.scss';
+
+main();
 
 render(<App />, document.querySelector("#app") as Element);
